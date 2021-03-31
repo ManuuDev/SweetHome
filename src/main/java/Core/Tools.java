0ecb070ab642aa0c37c52aed2b06fb95bca2ad72
manu <manuu.97@hotmail.com>
2021-03-31 14:59:01 -0300

    deleted test line

--- src/main/java/Core/Tools.java
+++ src/main/java/Core/Tools.java
@@ -59,9 +59,7 @@ public class Tools {
                     }
                 }
             }
-
-            ipAddress = null;
-
+            
             if (ipAddress == null)
                 throw new CustomException.NoIPV4();
 
